77f74b886e25424607c19957876509c74faaf2fe
akankshabaykarndss <akanksha.baykar@ndsofttech.com>
2026-03-10 16:29:16 +0530

    task2and 3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,19 @@
 import "../styles/Home.css"
 
+ const handleClick = () => {
+    console.log("Project Running!");
+  };
+
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>Home Page</h1>
+  
       <p>Welcome to the Home Page!</p>
       <h2>Hello, World!</h2>
+      <h1 className="name1">Hi ,Akanksha Baykar here😊</h1>
+
+      <button className="btn" onClick={handleClick}>Click Me</button>
+
     </div>
   )
 }
